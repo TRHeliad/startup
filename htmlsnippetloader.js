4dde17c4a59a2c0a51a896399cf758ed0dfbcab7
Ben Madsen <bmadsenonpc@gmail.com>
2024-02-14 16:10:08 -0700

    feat: add svg insertion with js

--- htmlsnippetloader.js
+++ htmlsnippetloader.js
@@ -2,8 +2,13 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
 document.getElementsByTagName('head')[0].appendChild(script);
 
+function load_svgs() {
+	$( ".checked-box" ).load( "checked-box.html" );
+	$( ".unchecked-box" ).load( "unchecked-box.html" );
+}
+
 window.onload = function ()
 {
-	$( "#header" ).load( "header.html" );
-	$( "#footer" ).load( "footer.html" );
+	$( "#header" ).load( "header.html", load_svgs );
+	$( "#footer" ).load( "footer.html", load_svgs );
 }
