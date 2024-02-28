ae6a013c63aaea2e7658cdb22c93e3fd9b54cf6b
Ben Madsen <bmadsenonpc@gmail.com>
2024-02-27 23:57:21 -0700

    feat: add local username saving

---
+++ scripts/login.js
@@ -0,0 +1,7 @@
+function login() {
+	const usernameElement = document.querySelector("#username");
+	const passwordElement = document.querySelector("#password");
+	localStorage.setItem("userName", usernameElement.value);
+	console.log("stored")
+	// For now the password will be ignored
+}
