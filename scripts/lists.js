7b3748a6fd4bcf798012c85401e8e74b74b88a02
Ben Madsen <bmadsenonpc@gmail.com>
2024-02-28 19:48:39 -0700

    feat: add list clearing and loading

--- scripts/lists.js
+++ scripts/lists.js
@@ -4,6 +4,37 @@ function getLists() {
 	return lists;
 }
 
+function clearLists() {
+	const tbodyElement = document.querySelector("tbody");
+	// Clear out lists table
+	const listElements = document.querySelectorAll("tr:not(#table-header)");
+	for (const rowElement of listElements) {
+		tbodyElement.removeChild(rowElement)
+	}
+}
+
+function loadLists() {
+	const lists = getLists();
+	const tbodyElement = document.querySelector("tbody");
+	// Add new lists
+	lists.forEach(function (list, i) {
+		const newNameCol = document.createElement("td");
+		newNameCol.textContent = list.Name;
+		const newCreatorCol = document.createElement("td");
+		newCreatorCol.textContent = list.Creator;
+		
+		const newRowElement = document.createElement("tr");
+		tbodyElement.appendChild(newRowElement);
+		newRowElement.appendChild(newNameCol);
+		newRowElement.appendChild(newCreatorCol);
+
+		newRowElement.addEventListener("click", function(event) {
+			localStorage.setItem("selectedList", i)
+			document.location.href = "/list.html"
+		})
+	})
+}
+
 function createList() {
 	const listNameElement = document.querySelector(".create-list-container #newList");
 	let listName = listNameElement.value;
@@ -12,4 +43,6 @@ function createList() {
 	let lists = getLists()
 	lists.push(newList);
 	localStorage.setItem("lists", JSON.stringify(lists));
+	clearLists()
+	loadLists()
 }
