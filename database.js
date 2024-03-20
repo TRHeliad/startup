2c6c96885762a8ad70dbed39838c3d8fb95d4248
Ben Madsen <bmadsenonpc@gmail.com>
2024-03-19 22:47:44 -0600

    feat: add basic user functions for database

--- database.js
+++ database.js
@@ -7,3 +7,40 @@ const url = `mongodb+srv://${dbConfig.userName}:${dbConfig.password}@${dbConfig.
 const client = new MongoClient(url);
 const db = client.db('simon');
 const userCollection = db.collection('user');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+	await client.connect();
+	await db.command({ ping: 1 });
+})().catch((ex) => {
+	console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+	process.exit(1);
+});
+
+function getUser(username) {
+	return userCollection.findOne({ username: username });
+}
+
+function getUserByToken(token) {
+	return userCollection.findOne({ token: token });
+}
+
+async function createUser(username, password) {
+	// Hash the password before we insert it into the database
+	const passwordHash = await bcrypt.hash(password, 10);
+
+	const user = {
+		username: username,
+		password: passwordHash,
+		token: uuid.v4(),
+	};
+	await userCollection.insertOne(user);
+
+	return user;
+}
+
+module.exports = {
+	getUser,
+	getUserByToken,
+	createUser
+};
