8be475cc232ee766902c391351b3fdb69d8360fc
Ben Madsen <bmadsenonpc@gmail.com>
2024-03-20 23:30:46 -0600

    feat: add /auth/login endpoint

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ apiRouter.post("/auth/create", async (req, res) => {
 	}
 });
 
+// GetAuth token for the provided credentials
+apiRouter.post("/auth/login", async (req, res) => {
+	const user = await DB.getUser(req.body.username);
+	if (user) {
+		if (await bcrypt.compare(req.body.password, user.password)) {
+			setAuthCookie(res, user.token);
+			res.send({ id: user._id });
+			return;
+		}
+	}
+	res.status(401).send({ msg: "Unauthorized" });
+});
+
 // getLists
 apiRouter.get("/lists/:username", (req, res) => {
 	const userLists = getLists(req.params.username);
