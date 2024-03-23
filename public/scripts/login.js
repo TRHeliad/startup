4e285953fd3ecc548e99712b163c44004e943774
Ben Madsen <bmadsenonpc@gmail.com>
2024-03-22 20:53:31 -0600

    feat: add loginOrCreate function

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -2,8 +2,29 @@ function login() {
 	const usernameElement = document.querySelector("#username");
 	const passwordElement = document.querySelector("#password");
 	localStorage.setItem("userName", usernameElement.value);
-	console.log("stored")
 	// For now the password will be ignored
 
-	document.location.href = "/lists.html"
+	document.location.href = "/lists.html";
 }
+
+async function loginOrCreate(endpoint) {
+	const username = document.querySelector("#username")?.value;
+	const password = document.querySelector("#password")?.value;
+
+	const response = await fetch(endpoint, {
+		method: "post",
+		body: JSON.stringify({ username: username, password: password }),
+		headers: {
+			"Content-type": "application/json; charset=UTF-8",
+		},
+	});
+
+	if (response.ok) {
+		localStorage.setItem("userName", username);
+		window.location.href = "lists.html";
+	} else {
+		// error display
+	}
+}
+
+window.addEventListener("load", function () {});
