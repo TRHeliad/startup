d25f95197a1dc06421d53e3baf6d56bf61cc1be7
Ben Madsen <bmadsenonpc@gmail.com>
2024-04-12 18:16:03 -0600

    feat: add lists view structure

--- src/lists/lists.jsx
+++ src/lists/lists.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import './lists.css';
+
 export function Lists() {
   return (
     <main>
-      <div>lists displayed here</div>
+		<h1>Lists</h1>
+		<table>
+			<tbody>
+				<tr id="table-header">
+					<th>Name</th>
+					<th>Creator</th>
+				</tr>
+			</tbody>
+		</table>
+		<div className="create-list-container">
+			<label>New List: </label>
+			<input id="newList" required pattern="\w{3,40}"/>
+			<button onclick="createList()">Create</button>
+		</div>
     </main>
   );
 }
