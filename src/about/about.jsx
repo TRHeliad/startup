bd92bce0743bd22afbc95876bf6273b7a588627f
Ben Madsen <bmadsenonpc@gmail.com>
2024-04-13 02:05:40 -0600

    feat: add about view

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import "./about.css";
+
 export function About() {
   return (
     <main>
-      <div>about displayed here</div>
-    </main>
+		<p>Hello. This website was made by Ben Madsen. Here is an AI generated image of him:</p>
+		<img src="assets/generated_portrait_square.jpg"/>
+	</main>
   );
 }
